e624d74a621e08e39946bb7c4c637db30aef8147
maaztausif <maaztausif48@gmail.com>
2020-06-02 22:55:27 +0500

    Initial Commit

---
+++ Machine_Learning_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine_Learning_Project
+//
+//  Created by Maaz Bin Tausif on 02/06/2020.
+//  Copyright © 2020 Maaz Bin Tausif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
